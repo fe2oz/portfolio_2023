2c93f9f2a0937f601c923911b382c2a5dcfb83bc
fe2oz <93234748+fe2oz@users.noreply.github.com>
2023-03-05 13:12:48 +0900

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// 변수 선언
+var header = document.querySelector('.at-header');
+
+// 스크롤 이벤트
+window.addEventListener('scroll', function(){
+  var scrollTop = window.scrollY;
+  // 스크롤 값이 헤더 높이 값보다 커지면 클래스 추가, 삭제
+  if(scrollTop >= header.offsetHeight){ 
+    header.classList.add('scroll');
+  }else{
+    header.classList.remove('scroll');
+  }
+});
