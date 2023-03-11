a65cb19676f70d13cadf73253736aa2e5393d915
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-03-11 17:06:39 +0900

    2023.03.11_17:06

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ var header = document.querySelector('.at-header');
 
 // 스크롤 이벤트
 window.addEventListener('scroll', function(){
-  var scrollTop = window.scrollY;
-  // 스크롤 값이 헤더 높이 값보다 커지면 클래스 추가, 삭제
-  if(scrollTop >= header.offsetHeight){ 
-    header.classList.add('scroll');
-  }else{
-    header.classList.remove('scroll');
-  }
+    var scrollTop = window.scrollY;
+    // 스크롤 값이 헤더 높이 값보다 커지면 클래스 추가, 삭제
+    if(scrollTop >= header.offsetHeight){ 
+        header.classList.add('scroll');
+    }else{
+        header.classList.remove('scroll');
+    }
 });
